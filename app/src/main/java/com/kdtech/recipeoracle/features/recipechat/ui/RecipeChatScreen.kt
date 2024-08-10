09049832a10b3348a7b6a2c217b1ab47b2f2dd3d
kushal dave <kushaldave2011@gmail.com>
2024-08-10 11:44:30 +0530

    added topbar in chat screen

--- app/src/main/java/com/kdtech/recipeoracle/features/recipechat/ui/RecipeChatScreen.kt
+++ app/src/main/java/com/kdtech/recipeoracle/features/recipechat/ui/RecipeChatScreen.kt
@@ -1,18 +1,27 @@
 package com.kdtech.recipeoracle.features.recipechat.ui
 
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import com.kdtech.recipeoracle.features.recipechat.presentation.models.RecipeChatState
 import com.kdtech.recipeoracle.features.recipechat.presentation.viewmodel.RecipeChatViewModel
+import com.kdtech.recipeoracle.resources.DrawableResources
+import com.kdtech.recipeoracle.resources.theme.RecipeTheme
 
 @Composable
 fun RecipeChatScreen(
@@ -23,17 +32,42 @@ fun RecipeChatScreen(
     val lazyColumnListState = rememberLazyListState()
 
     ConstraintLayout(
-        modifier = modifier.fillMaxSize()
+        modifier = modifier
+            .fillMaxSize()
             .padding(16.dp)
     ) {
-        val (chatListing, chatTextField) = createRefs()
-        LazyColumn(
+        val (topAppBar, chatListing, chatTextField) = createRefs()
+        Row(
             modifier = Modifier
-                .constrainAs(chatListing) {
+                .fillMaxWidth()
+                .constrainAs(topAppBar) {
                     top.linkTo(parent.top)
                     start.linkTo(parent.start)
                     end.linkTo(parent.end)
-                    bottom.linkTo(chatTextField.top)
+                },
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            IconButton(
+                onClick = viewModel::onBackPress
+            ) {
+                Icon(
+                    painter = painterResource(id = DrawableResources.back),
+                    contentDescription = "Back",
+                    tint = RecipeTheme.colors.black100
+                )
+            }
+            Text(
+                text = "Chat With Ai",
+                style = RecipeTheme.typography.robotoMedium,
+                color = RecipeTheme.colors.black100
+            )
+        }
+        LazyColumn(
+            modifier = Modifier
+                .constrainAs(chatListing) {
+                    top.linkTo(topAppBar.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
                 },
             state = lazyColumnListState
         ) {
--- app/src/main/java/com/kdtech/recipeoracle/resources/DrawableResources.kt
+++ app/src/main/java/com/kdtech/recipeoracle/resources/DrawableResources.kt
@@ -5,4 +5,5 @@ import com.kdtech.recipeoracle.R
 object DrawableResources {
     val clockIcon = R.drawable.baseline_access_time_filled_24
     val sendIcon = R.drawable.baseline_send_24
+    val back = R.drawable.back
 }
