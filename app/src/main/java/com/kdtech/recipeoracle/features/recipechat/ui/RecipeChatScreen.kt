3bd1b6c97ce8f1dabf2e308dcd80db1e779cfd18
kushal dave <kushaldave2011@gmail.com>
2024-08-14 18:12:32 +0530

    added auto scroll down when user is near bottom and opens keyboard

--- app/src/main/java/com/kdtech/recipeoracle/features/recipechat/ui/RecipeChatScreen.kt
+++ app/src/main/java/com/kdtech/recipeoracle/features/recipechat/ui/RecipeChatScreen.kt
@@ -4,9 +4,11 @@ import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -20,9 +22,11 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -33,6 +37,7 @@ import com.kdtech.recipeoracle.resources.StringResources
 import com.kdtech.recipeoracle.resources.theme.RecipeTheme
 import com.kdtech.recipeoracle.resources.theme.toHeightDp
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun RecipeChatScreen(
@@ -41,6 +46,9 @@ fun RecipeChatScreen(
 ) {
     val state by viewModel.state.collectAsState(RecipeChatState())
     val lazyColumnListState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+
+    val imeBottom = WindowInsets.ime.getBottom(LocalDensity.current)
 
     val checkingRecipeText: String = stringResource(StringResources.checkingRecipeBook)
     var typingText by remember { mutableStateOf(checkingRecipeText) }
@@ -59,6 +67,18 @@ fun RecipeChatScreen(
                 typingText = "$checkingRecipeText${typingDots[dotIndex % typingDots.size]}"
                 dotIndex++
                 delay(500L) // Delay to simulate typing effect
+            }
+        }
+    }
+
+    LaunchedEffect(imeBottom, state.chatList.size) {
+        if (imeBottom > 0) {
+            val lastVisibleItemIndex = lazyColumnListState.layoutInfo.visibleItemsInfo.lastOrNull()?.index
+            if (lastVisibleItemIndex != null && lastVisibleItemIndex >= state.chatList.size - 2) {
+                // Only auto-scroll if the user was already near the bottom
+                coroutineScope.launch {
+                    lazyColumnListState.animateScrollToItem(state.chatList.size - 1)
+                }
             }
         }
     }
