c4489a9a4484ca479585656a4333a62138a2f2d6
kushal dave <kushaldave2011@gmail.com>
2024-09-06 15:58:33 +0530

    code cleanup , removed unused prompt for getting ingridients list

--- app/src/main/java/com/kdtech/recipeoracle/prompt/Prompts.kt
+++ app/src/main/java/com/kdtech/recipeoracle/prompt/Prompts.kt
@@ -5,20 +5,6 @@ import com.kdtech.recipeoracle.common.Empty
 
 object Prompts {
     private const val AND_OR = "and/or"
-    fun getPromptForIngredients(): String {
-        return "Please generate a list of food ingredients in JSON format. " +
-                "Each item in the list should contain the following fields: " +
-                "\"name\" (the name of the ingredient), \"image_url\" (It should be empty string), " +
-                "and \"calories\" (the number of calories per 100 grams of the ingredient). " +
-                "The list should include a variety of common ingredients such as potato, tomato, onion, and others. " +
-                "Here is the structure for each item:\n" +
-                "{\n" +
-                "  \"name\": \"ingredient_name\",\n" +
-                "  \"image_url\": \"http://example.com/image.jpg\",\n" +
-                "  \"calories\": 0\n" +
-                "}\n\n" +
-                "Response should only contain JSON and nothing else."
-    }
 
     fun getPromptForChat(
         recipeName: String
